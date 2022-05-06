d7ab251236b5ac81fa0c5ed6174810befa9ea268
kokosro <robert@dnt.network>
2022-05-06 09:34:52 +0300

    DNT-176
    withdraw request function

--- contracts/DomainTokenBase.sol
+++ contracts/DomainTokenBase.sol
@@ -177,6 +177,16 @@ contract DomainTokenBase is ERC721Enumerable, Destroyable, IDomainTokenBase, Ini
     emit DomainFreeze(_chainId, tokenId, domains[tokenId].frozen);
   }
 
+  function withdraw(uint256 tokenId) external override {
+      require(_exists(tokenId), "Domain does no exist");
+      require(_isApprovedOrOwner(msg.sender, tokenId), "not owner of domain");
+      require(domains[tokenId].isNotLocked(), "Domain is locked");
+      require(domains[tokenId].isNotFrozen(), "Domain is frozen");
+      require(domains[tokenId].isNotExpired(), "Domain is expired");
+      domains[tokenId].setFreeze(true);
+      emit WithdrawRequest(_chainId, tokenId);
+  }
+
   function getDomainInfo(uint256 tokenId)
     external
     view
--- contracts/interfaces/IDomainTokenBase.sol
+++ contracts/interfaces/IDomainTokenBase.sol
@@ -37,7 +37,8 @@ interface IDomainTokenBase {
 
   event DomainFreeze(uint256 chainId, uint256 tokenId, uint256 status);
   event DomainLock(uint256 chainId, uint256 tokenId, uint256 status);
-
+  event WithdrawRequest(uint256 chainId, uint256 tokenId);
+  
   function getDomainInfo(uint256) external view returns (DataStructs.Domain memory);
 
   function setFreeze(uint256, bool) external;
@@ -49,4 +50,6 @@ interface IDomainTokenBase {
   function isLocked(uint256) external view returns (bool);
 
   function isFrozen(uint256) external view returns (bool);
+
+  function withdraw(uint256) external;
 }
